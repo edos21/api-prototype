8fcf5cff7dace458ccfa34bcbae89b1619453005
Eduardo Sanchez <edos21@gmail.com>
2020-01-22 19:47:24 -0300

    Modifies dependencies and adds POST api resource

--- src/app.py
+++ src/app.py
@@ -1,13 +1,28 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
+from datetime import date
+import uuid 
 
 app = Flask(__name__)
-api = Api(app)
 
-class Reviews(Resource):
-  def get(self, id):
-    return {'id': id}
+reviews = []
 
-api.add_resource(Reviews, '/v1/review/<string:id>')
+@app.route('/v1/review/', methods=['POST'])
+def create_review():
+  body = request.get_json()
+  if next(filter(lambda x: x['sale_id'] == body['sale_id'], reviews), None):
+    return {'message': "A review for the sale '{}' already exists.".format(body['sale_id'])}, 400
+  
+  review = {
+      "id": uuid.uuid1().int,
+      "sale_id": body['sale_id'],
+      "store_id": body['store_id'],
+      "user_id": body['user_id'],
+      "review": body['review'],
+      "score": body['score'],
+      "date": date.today(),
+      "is_deleted": False,
+  }
+  reviews.append(review)
+  return jsonify(review)
 
 app.run(port=5000)

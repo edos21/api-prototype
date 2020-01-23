3671f7cb55ddc88a3f0933e5a78931fb89585f64
Eduardo Sanchez <edos21@gmail.com>
2020-01-23 16:28:39 -0300

    fix on error messages and adds information

--- src/app.py
+++ src/app.py
@@ -1,20 +1,30 @@
+'''
+  App file for 'Pedidos Ya' Backend technical test.
+  This is the prototype implementation for the proposed scenario.
+  It was made with Python using Flask as framework, runing on http://127.0.0.1:5000/
+  it has no Database so all Reviews will be saved locally and will be lost when the
+  local server quits.
+
+  The variables and methods name used are self explanatory
+  for any doubt, don't hessitate on contacting me
+  Author: Eduardo Sanchez (edos21@gmail.com)
+
+  The API Documentation can be found at https://documenter.getpostman.com/view/257959/SWT8fe3i?version=latest
+'''
 from flask import Flask, request, jsonify
 from datetime import datetime, timedelta
 import uuid 
 
 app = Flask(__name__)
 
-reviews = []
+reviews = [] #local storage for the created Reviews
 
-@app.route('/v1/test/', methods=['GET'])
-def test():
-  return jsonify(reviews)
 
 @app.route('/v1/review/', methods=['POST'])
 def create_review():
   body = request.get_json()
   if next(filter(lambda x: x['sale_id'] == body['sale_id'], reviews), None):
-    return {'message': f"A review for the sale '{body['sale_id']}' already exists."}, 400
+    return {'message': f"Sale '{body['sale_id']}' has already been reviewed."}, 400
   
   review = {
       "id": uuid.uuid1().int,
@@ -26,6 +36,7 @@ def create_review():
       "date": datetime.today(),
       "is_deleted": False,
   }
+
   reviews.append(review)
   return jsonify(review)
 
@@ -35,14 +46,16 @@ def delete_review(id):
   if review:
     review["is_deleted"] = True
     return {'message': "The review was deleted!"}
-  return {'message': f"A review with the ID '{id}' does not exist."}, 404
+
+  return {'message': f"Review ID '{id}' does not exist."}, 404
 
 
 @app.route('/v1/sale/<int:sale_id>/review', methods=['GET'])
 def get_sale_review(sale_id):
     review = next(filter(lambda x: x['sale_id'] == sale_id and not x['is_deleted'], reviews), None)
     if not review:
-      return {'message': f"A review for the sale '{sale_id}' does not exist"}, 404
+      return {'message': f"Review for sale '{sale_id}' does not exist"}, 404
+
     return jsonify(review)
 
 
@@ -64,7 +77,7 @@ def get_store_reviews(store_id):
     and not x['is_deleted'], reviews))
 
     if not reviews_list:
-      return {'message': f"There is no reviews for the store '{store_id}' from date {start_date: %d/%m/%Y} to date {end_date: %d/%m/%Y}"}, 404
+      return {'message': f"There are no reviews for store '{store_id}' from date {start_date: %d/%m/%Y} to date {end_date: %d/%m/%Y}"}, 404
     return jsonify(reviews_list)
 
 app.run(port=5000)
